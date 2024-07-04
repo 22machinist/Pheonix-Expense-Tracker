90d8d3157980e83e3b88cd3ee3d3b1b44cd165a0
22machinist <sharmashutosh22@gmail.com>
2024-07-05 00:57:27 +0530

    Added the code

---
+++ expense-tracker/app/page.tsx
@@ -0,0 +1,34 @@
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+import AddTransaction from "@/components/AddTransaction";
+import Balance from "@/components/Balance";
+import IncomeExpense from "@/components/IncomeExpense";
+import TransactionList from "@/components/TransactionList";
+
+
+const HomePage = async () => {
+
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest></Guest>;
+  }
+
+  return (
+    <main>
+      <h2>
+        Welcome , {user.firstName}
+      </h2>
+      <Balance>
+
+      </Balance>
+      <IncomeExpense></IncomeExpense>
+      <AddTransaction>
+
+      </AddTransaction>
+      <TransactionList></TransactionList>
+    </main>
+  );
+}
+
+export default HomePage;
